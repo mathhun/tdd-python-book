9ecea2e176368d7b90931938aba4635926c0765b
mathhun <achemer@gmail.com>
2015-04-10 12:59:44 +0900

    Functional test now checks we can input a todo item

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -12,9 +13,42 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online todo app.
+        # She goes to check out its homepage
         self.browser.get('http://localhost:8000')
 
+        # She notices the page title and header mention todo lists
         self.assertIn('ToDo', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('ToDo', header_text)
+
+        # She is invited to enter a todo item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a todo item'
+        )
+
+        # She types "Buy peacock feathers" into a text box
+        # (Edith's hobby is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathres" as an item in a todo list table
+        inputbox.send_keys(Keys.Enter)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathres' for row in rows)
+        )
+
+        # There is still a text box inviting her to add another item.
+        # She enters "Use peacock feathres to make a fly"
+        # (Edith is very methodical)
+        self.fail('Finish the test!')
+
+        # The page updates again, and now shows both items on her list
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
