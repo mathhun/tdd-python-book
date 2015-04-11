91bc295314207600ab1b9d1413a3233a2b3b9e78
mathhun <achemer@gmail.com>
2015-04-12 02:20:43 +0900

    new tests

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -35,20 +35,33 @@ class NewVisitorTest(unittest.TestCase):
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathres" as an item in a todo list table
-        inputbox.send_keys(Keys.Enter)
+        inputbox.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathres' for row in rows)
-        )
+        rows = table.find_elements_by_tag_name('tr')
+        #self.assertTrue(
+        #    any(row.text == '1: Buy peacock feathres' for row in rows),
+        #    "New todo item did not appear in table -- its text was:\n%s" % (
+        #        table.text,
+        #    )
+        #)
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathres to make a fly"
         # (Edith is very methodical)
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on her list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        
+        self.fail('Finish the test!')
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
